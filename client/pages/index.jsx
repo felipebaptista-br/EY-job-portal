e4ba91b1aade9f827e67b28edb8f73bc778b1739
Felipe Baptista <felipe.baptista06@gmail.com>
2023-07-10 21:28:57 -0300

    add gitignore

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -20,7 +20,7 @@ export default function Home() {
     return (
         <>
             <main className={style['home-section']}>
-
+                
             </main>
             <Footer />
         </>
