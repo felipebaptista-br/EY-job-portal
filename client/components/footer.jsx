402fd7ec300a1f8d57f9d1781da1e50fa62551e6
Felipe Baptista <felipe.baptista06@gmail.com>
2023-07-10 22:24:01 -0300

    new page home

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -5,7 +5,7 @@ import style from "../styles/components/footer.module.css";
 export default function Footer() {
     return (
         <footer className={style.footer}>
-            <p>INFINITY - © 2023</p>
+            <p>© 2023 - Infinity Company</p>
         </footer>
     )
 }
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
+import Logo from "../images/logo.png";
+import Image from 'next/image';
+import Background from "../images/background-home-initial.jpg";
 import { useDispatch } from "react-redux";
 import { setUser } from "../utils/reducers/users";
 import Footer from "../components/footer";
+import Button from "../components/button";
 
 import style from "../styles/pages/home.module.css";
 
@@ -19,9 +23,29 @@ export default function Home() {
 
     return (
         <>
-            <main className={style['home-section']}>
-                
-            </main>
+            <div className={style['home-initial']}>
+                <section className={style['home-initial-contents']}>
+                    <Image
+                        src={Logo}
+                        className={style.logo}
+                        alt="Background página Home"
+                    />
+                    <article>
+                        <h1 className={style['home-initial-title']}>A vaga ideal para você, na palma da sua mão.</h1>
+                        <p className={style['home-initial-content']}>Somos uma plataforma inteligênte com mais de 250 vagas! Nosso objetivo é facilitar o processo de recrutamento e seleção fazendo um papel de ponte entre recrutadores e candidatos.</p>
+                    </article>
+                    <Button
+                        children='ABRA SUA CONTA'
+                        route='/login'
+                        style={{ padding: '1rem 5rem', marginTop: '3rem' }}
+                    />
+                </section>
+                <Image
+                    src={Background}
+                    className={style['background-home-initial']}
+                    alt="Background página Home"
+                />
+            </div>
             <Footer />
         </>
     )
